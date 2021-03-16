8c89964287c20f0146861412bb2ccef30eb1beeb
Kostas Zafolias <76820150+Daewooer@users.noreply.github.com>
2021-03-16 12:50:34 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Hello, what is youre name?")
+user_name = input()
+
+if user_name == "Klaountio":
+    print("Ante re blaka")
+
+elif user_name == "Nino":
+    print("Mhn baras thn Sinti re")
+
+elif user_name == "Boris":
+    print("Boris make new video")
+
+elif user_name == "OP VINCENZO":
+    print("Klaountio is liking u, weirdly")
+
+else:
+    print("Hello " + user_name + " have a nice day :)")
